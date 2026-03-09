72528d961ee262b7ba7345a17a5249d6088ab416
Philip-Lucky <philipslucky24@gmail.com>
2026-03-09 06:10:22 -0700

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -2,20 +2,20 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 6.35.0" # Access to 2026 features
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = "~> 1.14.0"
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "eu-north-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+  ami           = "ami-073130f74f5ffb161" #valid for eu-north-1
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Terraform_Demo"
